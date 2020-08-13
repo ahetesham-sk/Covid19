4e5c01fcb15fa18d19fc526044743f6274f4f397
Ahetesham Shaikh <ahetesham.ma@outlook.com>
2020-08-13 17:37:58 +0530

    upload pdf code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 @Component({
   selector: 'app-root',
--- src/app/pages/blogs/blogs.component.ts
+++ src/app/pages/blogs/blogs.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { BlogService } from 'src/app/services/blog.service';
 import { ResponseModel } from 'src/app/model/response.model';
 
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
+
 @Component({
   selector: 'app-blogs',
   templateUrl: './blogs.component.html',
@@ -11,6 +15,36 @@ export class BlogsComponent implements OnInit {
 
   public blogs: any = {};
   response: any;
+  public employee = [
+    {
+      id:1,
+      name:'Ahetesham Shaikh',
+      mobile:'9403381578',
+      address : '336, Shani Peth, Jalgaon (425001).',
+      state:'Maharashtra'
+    },
+    {
+      id:2,
+      name:'Balasaheb Patil',
+      mobile:'8087562010',
+      address : 'Flat 12, A wing New BJ Market Jalgaon (425001).',
+      state:'Maharashtra'
+    },
+    {
+      id:3,
+      name:'Junaid Khan',
+      mobile:'9096168614',
+      address : 'Shani Peth Jalgaon (425001).',
+      state:'Maharashtra'
+    },
+    {
+      id:4,
+      name:'Salim Shaikh',
+      mobile:'9096168614',
+      address : 'Shani Peth Jalgaon (425001).',
+      state:'Maharashtra'
+    }
+  ]
   constructor(private _blogService: BlogService) { }
 
   ngOnInit() {
@@ -26,5 +60,12 @@ export class BlogsComponent implements OnInit {
         console.log(error);
       })
   }
+
+  generatePdf(){
+    const documentDefinition = { content: 'This is for testing.' };
+    pdfMake.createPdf(documentDefinition).open();   //To Open PDF
+   // pdfMake.createPdf(documentDefinition).download(); //To Download PDF
+  }
+
 
 }
